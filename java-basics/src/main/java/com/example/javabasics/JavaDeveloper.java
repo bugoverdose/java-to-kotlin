7957d55bdbbe6a37d3f1576504cf019928c7f72b
Jinwoo Jeong <bugod96@gmail.com>
2021-08-17 18:52:59 +0900

    minor changes

--- java-basics/src/main/java/com/example/javabasics/JavaDeveloper.java
+++ java-basics/src/main/java/com/example/javabasics/JavaDeveloper.java
@@ -1,9 +1,9 @@
 package com.example.javabasics;
 
 public class JavaDeveloper {
-    final public String name; // 자바의 final == 코틀린의 val 선언. JS의 const 선언과 유사. // 세터 자동 생성도 예방됨
-    public int age;
-    public String message = "응애! 나 애기 자바 개발자!";
+    final private String name; // 자바의 final == 코틀린의 val 선언. JS의 const 선언과 유사. // 세터 자동 생성도 예방됨
+    private int age;           // private이므로 직접 수정 불가
+    private String message = "응애! 나 애기 자바 개발자!";
 
     // 생성자 기본 형식: 클래스명과 동일한 이름. 반환형이 없음(언제나 해당 인스턴스를 반환).
     // constructor overloading : 복수의 생성자 정의. 매개변수 구성의 차이.
